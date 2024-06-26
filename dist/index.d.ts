6c176b57370d917f451239a8cb2eb1485eb04453
Cat Chen <catchen@catchen.me>
2024-06-26 05:43:07 +0000

    Getting information for User or Bot (#1)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,25 @@
+type User = {
+    login: string;
+    globalId: string;
+    id: number;
+    /**
+     * May be `undefined` if the user has not set a name.
+     */
+    name?: string;
+    /**
+     * May be `undefined` if the user chose to hide their email.
+     */
+    email?: string;
+    type: 'User';
+};
+type Bot = {
+    login: string;
+    appSlug: string;
+    globalId: string;
+    id: number;
+    name: string;
+    email: string;
+    type: 'Bot';
+};
+export declare function run(): Promise<User | Bot>;
 export {};
